a3190efeccfe44ea2c12bb7c8af8942e5fa43d7f
FM <Dev02@FMs-MacBook-Air.local>
2014-03-05 03:54:35 -0800

    Initial Commit

---
+++ Git Test/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  Git Test
+//
+//  Created by FM on 3/5/14.
+//  Copyright (c) 2014 FM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by FM on 3/5/14.
+//  Copyright (c) 2014 FM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
